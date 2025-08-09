b704665ac05a954f4189e51489c5496b4c5972fb
Martha Angelica Purba <martha.angelicaprb@gmail.com>
2025-08-09 22:04:45 +0700

    Update script.js
    delete file

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.getElementById("absensiForm").addEventListener("submit", function(e) {
-    e.preventDefault();
 
-    let nama = document.getElementById("Nama").value;
-    let nim = document.getElementById("NIM").value;
-    let keterangan = document.getElementById("status").value;
-
-    let tabel = document.getElementById("tabelAbsensi").getElementsByTagName('tbody')[0];
-    let row = tabel.insertRow();
-
-    let no = tabel.rows.length; // auto nomor urut
-    row.insertCell(0).innerText = no;
-    row.insertCell(1).innerText = nim;
-    row.insertCell(2).innerText = nama;
-    row.insertCell(3).innerText = keterangan;
-
-    // reset form
-    document.getElementById("absensiForm").reset();
-});
